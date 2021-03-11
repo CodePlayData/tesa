48202f3d0c933171bb868eb972989911dc191a03
Pedro Paulo dos Santos <dr2p@hotmail.com>
2021-03-11 14:44:44 -0300

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,10 @@
 import { parse as parseCsv } from 'https://deno.land/std@0.82.0/encoding/csv.ts'
 
 self.onmessage = async (e) => {
-    const { filename, alias } = e.data;
-    const result = await parseCsv(await Deno.readTextFile(filename), { skipFirstRow: true, separator: ";" })
-    postMessage(result.filter(place => place.Alias === alias).map(place => place.Link))
+    const { filename, alias, type } = e.data;
+    const content = await parseCsv(await Deno.readTextFile(filename), { skipFirstRow: true, separator: ";" }),
+		result = content.filter(place => place.Alias === alias).map(place => place.Link)
+    
+	console.log({ "alias": alias, "type": type, "result": result })
     self.close()
-  };
+  }
