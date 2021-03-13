348eb5e9755b2149d1e1ad9a2fdbf592fc35d03f
Pedro Paulo dos Santos <dr2p@hotmail.com>
2021-03-13 17:03:57 -0300

    Update controllers.js

--- src/controllers.js
+++ src/controllers.js
@@ -1,38 +1,23 @@
 // all the application logic must be here.
 const 
-    getPlace = ({ params, response }) => {
+    getPlace = async ({ params, response }) => {
         
         let { type, alias } = params
         let worker = new Worker(new URL("./worker.js", import.meta.url).href, { type: "module", deno: { namespace: true } })
         
-        worker.addEventListener('message',  async (e) => {
-            const ab2str = (buff) => {
-                let arr = new Uint16Array(buff)
-                let retVal = ""            
-                for(var idx = 0, len = arr.length; idx < len; idx += 65535){
-                    if(idx + 65535 > len){
-                        retVal += String.fromCharCode.apply(null, arr.subarray(idx, idx + (len - idx)))
-                    }
-                    else{
-                        retVal += String.fromCharCode.apply(null, arr.subarray(idx, idx + 65535))
-                    }
-                }         
-                return retVal
-            }  
-
-            let result = await Promise.resolve(
-                ab2str(Object.values(...e.data))
-            )
-            console.log(result)
-        })
-
+	    await  worker.addEventListener('message',  async (e) => {console.log(e.data)})
+        
         const sendToWork = (worker, type, alias) => {
-                worker.postMessage({ filename: "./src/data/" + type + "_list.csv", type, alias })
+                worker.postMessage({ filename: "./" + type + "_list.csv", type, alias })
                 console.log('\nData is already in the worker\n')
         }
         
         sendToWork(worker,type,alias)
-    },
+
+	    const decoder = new TextDecoder("utf-8");
+        const data = await Deno.readFile("teste.txt")
+	response.body = decoder.decode(data)
+  },
     postDataset = (ctx) => {
         ctx.response.body = "postDataset"
     },
