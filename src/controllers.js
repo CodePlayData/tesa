64ff027855f38173905d302c8da4298c037eebdb
Pedro Paulo dos Santos <dr2p@hotmail.com>
2021-03-13 17:30:39 -0300

    Update controllers.js

--- src/controllers.js
+++ src/controllers.js
@@ -4,19 +4,15 @@ const
         
         let { type, alias } = params
         let worker = new Worker(new URL("./worker.js", import.meta.url).href, { type: "module", deno: { namespace: true } })
-        
-	    await  worker.addEventListener('message',  async (e) => {console.log(e.data)})
-        
-        const sendToWork = (worker, type, alias) => {
-                worker.postMessage({ filename: "./" + type + "_list.csv", type, alias })
-                console.log('\nData is already in the worker\n')
-        }
-        
-        sendToWork(worker,type,alias)
-
-	    const decoder = new TextDecoder("utf-8");
-        const data = await Deno.readFile("teste.txt")
-	response.body = decoder.decode(data)
+        let data
+	
+	const decoder = new TextDecoder("utf-8")
+	
+	worker.postMessage({ filename: "./" + type + "_list.csv", type, alias })
+	    
+	await worker.onMessage = async (e) => ({data: await Deno.readFile(e.data.filename)})
+	
+	response.body = await decoder.decode(data)
   },
     postDataset = (ctx) => {
         ctx.response.body = "postDataset"
