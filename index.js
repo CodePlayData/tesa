14cbe0b9bea669566479eee6b27972a712376293
Pedro (W) <dr2p@hotmail.com>
2021-03-13 23:53:32 -0300

    ...

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ async function getCountryPolygon (alias) {
 
 }
 
+async function getMacroregionPolygon (alias) {
+    
+    const fetched_data = fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/country_list.csv")
+    const raw_data = await fetched_data
+    const raw_table = await raw_data.text()
+    const country_list = await parseCsv(raw_table, { skipFirstRow: true, separator: ";" })
+    const result = fetch(...country_list.filter(place => place.Alias === alias).map(place => place.Link)) 
+
+    let polygon
+    
+    polygon = await (await result).json()
+    console.log(polygon)
+
+}
+
 
 
 getCountryPolygon('BR')
