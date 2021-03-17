787d95d43e2c3c6669a0d19a5e79defafd643521
Pedro (W) <dr2p@hotmail.com>
2021-03-16 23:03:02 -0300

    ...

--- index.js
+++ index.js
@@ -2,48 +2,79 @@ import { parse as parseCsv } from 'https://deno.land/std@0.82.0/encoding/csv.ts'
 
 async function getCountryPolygon (alias) {
     
-    const fetched_data = fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/country_list.csv")
-    const raw_data = await fetched_data
-    const raw_table = await raw_data.text()
-    const country_list = await parseCsv(raw_table, { skipFirstRow: true, separator: ";" })
-    const result = fetch(...country_list.filter(place => place.Alias === alias).map(place => place.Link)) 
+    const list = 
+        await parseCsv(
+            await (
+                await fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/country_list.csv"))
+                .text(), { skipFirstRow: true, separator: ";" }
+                )
+    
+    const result = 
+        fetch(...list.filter(place => place.Alias === alias.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, ""))
+            .map(place => place.Link))
 
     let polygon
     
-    polygon = await (await result).json()
+    polygon = 
+        await (
+            await result)
+            .json()
+
     console.log(polygon)
 
 }
 
 async function getMacroregionPolygon (alias) {
     
-    const fetched_data = fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/macroregion_list.csv")
-    const raw_data = await fetched_data
-    const raw_table = await raw_data.text()
-    const country_list = await parseCsv(raw_table, { skipFirstRow: true, separator: ";" })
-    const result = fetch(...country_list.filter(place => place.Alias === alias).map(place => place.Link)) 
+    const list = 
+        await parseCsv(
+            await (
+                await fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/macroregion_list.csv"))
+                .text(), { skipFirstRow: true, separator: ";" }
+                )
+    
+    const result = 
+        fetch(...list.filter(place => place.Alias === alias.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, ""))
+            .map(place => place.Link))
 
     let polygon
     
-    polygon = await (await result).json()
+    polygon = 
+        await (
+            await result)
+            .json()
+            
     console.log(polygon)
 
 }
 
 async function getStatesPolygon (alias) {
     
-    const fetched_data = fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/macroregion_list.csv")
-    const raw_data = await fetched_data
-    const raw_table = await raw_data.text()
-    const country_list = await parseCsv(raw_table, { skipFirstRow: true, separator: ";" })
-    const result = fetch(...country_list.filter(place => place.Alias === alias).map(place => place.Link)) 
+    const list = 
+        await parseCsv(
+            await (
+                await fetch("https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/states_list.csv"))
+                .text(), { skipFirstRow: true, separator: ";" }
+                )
+    
+    const result = 
+        fetch(...list.filter(place => place.Alias === alias.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, ""))
+            .map(place => place.Link))
 
     let polygon
     
-    polygon = await (await result).json()
+    polygon = 
+        await (
+            await result)
+            .json()
+
     console.log(polygon)
 
 }
 
 getCountryPolygon('BR')
 getMacroregionPolygon('centro-oeste')
+getStatesPolygon('11')
