77994cb23de7345c9e7ce846086a3c67ddd289f3
Pedro (W) <dr2p@hotmail.com>
2021-03-29 01:05:09 -0300

    ...

--- mod.test.js
+++ mod.test.js
@@ -171,6 +171,51 @@ Deno.test({
 })
 
 Deno.test({
+    name: "getManyImmediateTest", 
+    fn: async () => {
+        let polygon = await getManyPolygons({ type: "immediate", aliases: ["CACOAL", "PARINTINS", "BRAGANCA", "CAPANEMA", "TUCURUI"] })
+        
+        assertObjectMatch( 
+            polygon[0],
+            { properties: {
+                codarea: "110005"
+                }
+            })
+            
+        assertObjectMatch( 
+            polygon[1],
+            { properties: {
+                codarea: "130010"
+                }
+            })
+            
+        assertObjectMatch( 
+            polygon[2],
+            { properties: {
+                codarea: "150005"
+                }
+            })
+        
+        assertObjectMatch( 
+            polygon[3],
+            { properties: {
+                codarea: "150006"
+                }
+            })
+            
+        assertObjectMatch( 
+            polygon[4],
+            { properties: {
+                codarea: "150011"
+                }
+            })
+        
+        } 
+})
+
+
+
+Deno.test({
     name: "getManyMiddlesTest", 
     fn: async () => {
         let polygon = await getManyPolygons({ type: "middleregions", aliases: ["BAURU", "BORBOREMA", "ITAPETININGA"] })
