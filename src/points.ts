4f77a35c656d624204792be3904f65fdefea610f
Pedro Paulo dos Santos <dr2p@hotmail.com>
2022-02-20 17:37:21 -0300

    falta incluir os binários

---
+++ src/points.ts
@@ -0,0 +1,128 @@
+// @filename: points.ts
+
+import { parseCsv } from "../deps.ts";
+import type {
+  Config,
+  GeoJsonPoint,
+  Layout,
+  ListItems,
+  LocationAddress,
+  LocationPosition,
+} from "../types.d.ts";
+
+/**
+ * The geocoding process is highly dependent to the Map Tiles server. We can use the World Map in Nominatim/OpenStreetMap, setup a private
+ * server for a whole country or a server for each geographic region; we could also request the location based in the raw string or in a structured objet. 
+ * The params of this function deal with all these options.
+ *
+ * @param layout defines if the request will be raw string (unstructured) or an object that will store address information, labels and urls of the private servers.
+ * @param location A structured object with housenumber, street, state and city, or a raw string with the address.
+ * @param config Is a module piece that, besides a bunch of information, defines were
+ * to find a CSV list that will store the states of which the requests belongs and directs to the proper private server identified by labels.
+ * @returns A GeoJson object of Point type or a empty array(_not located_).
+ */
+async function forwardGeocoding(
+  layout: Layout,
+  location: LocationAddress | string,
+  config?: Config,
+): Promise<GeoJsonPoint | undefined> {
+  var mapTiles: Layout["mapTiles"];
+  let request: Layout["request"];
+
+  mapTiles = layout.mapTiles;
+
+  if (Array.isArray(mapTiles)) {
+    const listURL = config?.lists?.main?.states ??
+        "https://raw.githubusercontent.com/CodePlayData/tesa/main/src/data/states_list.csv",
+      { housenumber, street, city, state } = location as LocationAddress;
+
+    try {
+      const list = await parseCsv(
+        await (
+          await fetch(listURL)
+        )
+          .text(),
+        { skipFirstRow: true, separator: ";" },
+      ) as ListItems[];
+
+      const result = mapTiles.filter((server) =>
+        server.name ===
+          list.filter((place) =>
+            place.Alias === state
+              .toUpperCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+          )[0].Subset
+      )[0] as { name: string; url: string };
+
+      console.log(`Requesting to ${result.name} a structured query...`);
+      return (await JSON.parse(
+        await (await fetch(
+          `${result.url}/search?street=${housenumber} ${street}&city=${city}&state=${state}&country=Brazil&format=geojson`,
+        )).text(),
+      )) as GeoJsonPoint;
+    } catch (error) {
+      console.log(error.message);
+    }
+  } else {
+    request = layout.request;
+
+    const name = mapTiles?.name ?? "nominatim";
+    const url = mapTiles?.url ?? "https://nominatim.openstreetmap.org";
+
+    try {
+      if (request === "unstructured") {
+        console.log(`Requesting to ${name} a unstructured query...`);
+        return (await JSON.parse(
+          await (await fetch(`${url}/search?q=${location}&format=geojson`))
+            .text(),
+        ));
+      } else {
+        const { housenumber, street, city, state } =
+          location as LocationAddress;
+
+        console.log(`Requesting to ${name} a structured query...`);
+        return (await JSON.parse(
+          await (await fetch(
+            `${url}/search?street=${housenumber} ${street}&city=${city}&state=${state}&country=Brazil&format=geojson`,
+          )).text(),
+        )) as GeoJsonPoint;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+}
+
+/**
+ * The reverse geocoding map only works for the whole country since beforehand you don't know witch states belongs the lat and lon code.
+ * However, you can select the private server or lets the nominatim.
+ * @param layout contain only information with will be requested to a private server or nominatim server.
+ * @param location is the latitude and longitude of the location.
+ * @returns A Geojson object of Point type or a empty array(_not located_).
+ */
+async function reverseGeocoding(
+  layout: Layout,
+  location: LocationPosition,
+): Promise<GeoJsonPoint | undefined> {
+  var lat: LocationPosition["lat"],
+    lon: LocationPosition["lon"],
+    mapTiles: Layout["mapTiles"];
+
+  mapTiles = layout.mapTiles as { name?: string; url?: string };
+  lat = location.lat;
+  lon = location.lon;
+
+  const url = mapTiles?.url ?? "https://nominatim.openstreetmap.org";
+
+  try {
+    return (await JSON.parse(
+      await (await fetch(`${url}/reverse?format=geojson&lat=${lat}&lon=${lon}`))
+        .text(),
+    ));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export { forwardGeocoding, reverseGeocoding };
---
+++ src/utils.ts
@@ -0,0 +1,138 @@
+// filename: utils.ts
+/**
+ * Some functions to facilitate to get geolocation jobs.
+ */
+ import { tgz, readerFromStreamReader } from "../deps.ts";
+ import { belongsTo, getOnePolygon } from "../mod.ts";
+ 
+ /**
+  * A function to download anything and save in filesystem.
+  * @param url the external url to download.
+  * @param destfile the path for save file.
+  */
+ export async function downloadFile(url: string, destfile: string) {
+     const responseObject = await fetch(url);
+     const ResponseReadableStream = responseObject.body?.getReader();
+ 
+     if(ResponseReadableStream) {
+         const readableStream = readerFromStreamReader(ResponseReadableStream);
+         const file=await Deno.open(destfile, {create: true, write: true});
+         await Deno.copy(readableStream, file);
+         file.close();
+     }
+ };
+ 
+ /**
+  * A function to untar files.
+  * @param path the file to be untar.
+  * @param destfile the path where the content will be saved.
+  */
+ export async function untarFile(path: string, destfile: string) {
+     await tgz.uncompress(path, destfile)
+ };
+ 
+ /**
+  * A function to run shell command in child process.
+  * @param cmd a shell command string to run in a child process.
+  * @param print bollen that defines if the result is to be printed in the stdout.
+  */
+ export async function runCmd(cmd: string, print?: boolean) {      
+     const p = Deno.run({
+         cmd: cmd.split(' '),
+         stdout: "piped",
+         stderr: "piped"
+     })
+     const { code } = await p.status();
+ 
+     // Reading the outputs closes their pipes
+     const rawOutput = await p.output();
+     const rawError = await p.stderrOutput();
+ 
+     if (code === 0 && print == true) {
+         await Deno.stdout.write(rawOutput);
+     } else {
+         const errorString = new TextDecoder().decode(rawError);
+         console.log(errorString);
+     }
+ 
+     Deno.exit(code);
+ };
+ 
+ /**
+  * This function gets de osmextractool from github and untar in the filesystem. However, this repo has already the bin files for use in the respective folder.
+  */
+ export async function getOsmExtractTools() {
+     const urls = {
+         extract_osm: "https://github.com/AndGem/osm_extract_polygon/releases/download/v.0.3.3/osm_extract_polygon-linux-amd64.tar.gz",
+     } as { [key: string]: string };
+ 
+     await downloadFile(
+         urls.extract_osm,
+         './osm_extract.tar.gz'
+     )
+ 
+     await untarFile('./osm_extract.tar.gz', '.')
+     console.log('Now install osmium-tools with:\n "sudo apt install osmium-tool"')
+ };
+ 
+ /**
+  * A function that extracts the pbf from a specific city.
+  * @param city a city to get the pbf file. Some cities need to be edited since there are double names.
+  */
+ export async function getCityPbf(city: string) {
+ 
+     try {
+         const metadata:any = await belongsTo(city, 'cities');
+         const polygon:any = await getOnePolygon(city, 'cities');
+         const City:string = city.replace(/\s+/g, '').normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+         
+         const url = {
+             Sul: "https://download.geofabrik.de/south-america/brazil/sul-latest.osm.pbf",
+             Sudeste: "https://download.geofabrik.de/south-america/brazil/sudeste-latest.osm.pbf",
+             Norte: "https://download.geofabrik.de/south-america/brazil/norte-latest.osm.pbf",
+             Nordeste: "https://download.geofabrik.de/south-america/brazil/nordeste-latest.osm.pbf",
+             'Centro-Oeste': "https://download.geofabrik.de/south-america/brazil/centro-oeste-latest.osm.pbf"
+         } as { [key: string]: string };
+     
+         await Promise.all(
+             [
+                 await Deno.writeTextFile(`${City}.geojson`, JSON.stringify(polygon)),
+                 await downloadFile(url[metadata.microrregiao.mesorregiao.UF.regiao.nome],`./${metadata.microrregiao.mesorregiao.UF.regiao.nome}-latest.osm.pbf`),               
+                 await runCmd(`./src/bin/osmium extract --strategy smart -p ${City}.geojson ${metadata.microrregiao.mesorregiao.UF.regiao.nome}-latest.osm.pbf -o ${City}.pbf`)
+             ]
+         ) 
+     } catch (error) {
+         console.log(error.message)
+     };
+ };
+ 
+ /**
+  * A function to get all neighboorhods from a file. 
+  * @param path the path of city pbf file.
+  */
+ export async function extractNeighboorhodFromPbf(path: string) {
+     await runCmd(`./src/bin/osm_extract_polygon -f ${path} --geojson -o --max 10 --min 10`, false)
+ };
+ 
+ /**
+  * A function to resume all neighborhood in one output, a geojson Feature Collection.
+  * @param path from all files of neighborhoods.
+  * @returns a geojson feature collection.
+  */
+ export async function readNeighborhoodGeojsonFromDir(path: string) {
+     let dir:string;
+     let features:any[] = []
+ 
+     for await (const dirEntry of Deno.readDir(path)) {
+         if(dirEntry.isFile && dirEntry.name.match(".geojson$")){
+             features.push(await JSON.parse(await Deno.readTextFile(`./${path}/${dirEntry.name}`)))
+         }
+       }
+     
+     //ToDo: Check the polygons that will be included and excluded depend on the neighborhood_list and neighborhood_includes.
+     const geojson = await JSON.stringify({ type: "FeatureCollection", features })
+     return geojson
+ };
+ 
+ //const bairros = await readNeighborhoodGeojsonFromDir('./riodejaneiro.pbf_polygons/')
+ 
