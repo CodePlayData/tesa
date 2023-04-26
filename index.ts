01572211ce9e5d0273ea0c7c273ffb90fe875f9a
dr2pedro <dr2p@hotmail.com>
2023-04-26 20:46:56 -0300

    Esperando a definição do ValueObject do Address no pacote fhir hl7.

---
+++ index.ts
@@ -0,0 +1,11 @@
+
+const brasil = new Country('brasil');
+
+brasil.find('')
+
+const rioDeJaneiro = new State('rio de janeiro');
+
+rioDeJaneiro.belongsTo()
+
+const me = new PointLocation().whereAmI()
+
